a8761671974e7b223d07ae319f8f0fd782214a55
Thales Baliero Takao <engc.thales@gmail.com>
2022-05-24 20:45:26 -0300

    feat: Estrutura do service criada para validação.

---
+++ correntista/src/main/java/br/com/itau/correntista/services/impl/ClienteService.java
@@ -0,0 +1,22 @@
+package br.com.itau.correntista.services.impl;
+
+import java.util.List;
+
+import br.com.itau.correntista.exceptions.AgenciaInvalidaException;
+import br.com.itau.correntista.models.Cliente;
+import br.com.itau.correntista.services.IClienteService;
+
+public class ClienteService implements IClienteService {
+
+	@Override
+	public void gravar(Cliente cliente) throws AgenciaInvalidaException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public List<Cliente> listarClientes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
